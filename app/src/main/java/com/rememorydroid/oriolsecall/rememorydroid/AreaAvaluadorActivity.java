9f1c2d310b22d47ad6c3ba5648b4d50fc28ae35f
osecall <osecall@uoc.edu>
2017-03-23 17:01:26 +0100

    S'ha agregat la part de autenticació per part de l'avaluador. Es controla si el email està verificat un cop creat. Quan es fa LogIn es porta a la pantalla de l'àrea de l'avaluador. S'ha agregat el menú bàsic on hi ha l'opció 'Sign Out' per acabar sessió de l'avaluador S'han agregat strings en multidiome

---
+++ app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/AreaAvaluadorActivity.java
@@ -0,0 +1,53 @@
+package com.rememorydroid.oriolsecall.rememorydroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AreaAvaluadorActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_area_avaluador);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menupostavaluador, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.btIniciMenu) {
+
+            //Retorna a la pantalla inicial
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(AreaAvaluadorActivity.this, R.string.signed_out,
+                    Toast.LENGTH_LONG).show();
+            Intent areaAvaluador = new Intent(AreaAvaluadorActivity.this, IniciActivity.class);
+            startActivity(areaAvaluador);
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/MainActivity.java
+++ app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton btcaIdioma, btesIdioma;
+    private ImageButton btcaIdioma, btesIdioma, btenIdioma;
     private Locale locale;
     private Configuration config = new Configuration();
 
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         btcaIdioma = (ImageButton) findViewById(R.id.btcaIdioma);
         btesIdioma = (ImageButton) findViewById(R.id.btesIdioma);
+        btenIdioma = (ImageButton) findViewById(R.id.btenIdioma);
 
         btcaIdioma.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,25 +31,39 @@ public class MainActivity extends AppCompatActivity {
                 locale = new Locale("ca");
 
                 Toast.makeText(MainActivity.this,
-                        "Ha sel·leccionat Català", Toast.LENGTH_SHORT).show();
+                        "Ha sel·leccionat Català", Toast.LENGTH_LONG).show();
                 config.setLocale(locale);
                 getResources().updateConfiguration(config,null);
-                Intent refresh = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(refresh);
+                Intent inici = new Intent(MainActivity.this, IniciActivity.class);
+                startActivity(inici);
             }
         });
 
         btesIdioma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                locale = new Locale("es");
+                locale = new Locale("es","ES");
 
                 Toast.makeText(MainActivity.this,
-                        "Ha seleccionado Español", Toast.LENGTH_SHORT).show();
+                        "Ha seleccionado Español", Toast.LENGTH_LONG).show();
                 config.setLocale(locale);
                 getResources().updateConfiguration(config,null);
-                Intent refresh = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(refresh);
+                Intent inici = new Intent(MainActivity.this, IniciActivity.class);
+                startActivity(inici);
+            }
+        });
+
+        btenIdioma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locale = new Locale("en");
+
+                Toast.makeText(MainActivity.this,
+                        "You chose English as a Language", Toast.LENGTH_LONG).show();
+                config.setLocale(locale);
+                getResources().updateConfiguration(config,null);
+                Intent inici = new Intent(MainActivity.this, IniciActivity.class);
+                startActivity(inici);
             }
         });
 
