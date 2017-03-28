7ad1355b72e2f7f63e81f50063f7ce408f52d925
osecall <osecall@uoc.edu>
2017-03-28 09:29:06 +0200

    S'ha agregat activity Guide

--- app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/GuiaActivity.java
+++ app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/GuiaActivity.java
@@ -1,16 +1,29 @@
 package com.rememorydroid.oriolsecall.rememorydroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GuiaActivity extends AppCompatActivity {
+
+    private Button btBackGuide;
+    private Intent GuideIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guia);
 
+        btBackGuide = (Button) findViewById(R.id.btBackGuide);
 
+        btBackGuide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GuideIntent = new Intent(GuiaActivity.this, TractamentsActivity.class);
+            }
+        });
 
 
 
