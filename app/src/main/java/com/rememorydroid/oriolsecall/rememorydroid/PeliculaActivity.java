f714145d3e9f66581ebc2903f9e3da8a113634f3
osecall <osecall@uoc.edu>
2017-03-30 01:26:55 +0200

    Botons selecció questionari

--- app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/PeliculaActivity.java
+++ app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/PeliculaActivity.java
@@ -20,10 +20,13 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 public class PeliculaActivity extends AppCompatActivity {
 
     private TextDrawable FromPage, ToPage;
-    private ImageView ivFromPage, ivToPage, ivOpcio1,ivOpcio2,ivOpcio3,ivOpcio4;
+    private ArrayList<TextDrawable> botons = new ArrayList<TextDrawable>();
+    private ImageView ivFromPage, ivToPage,ivOpcio1,ivOpcio2,ivOpcio3,ivOpcio4,ivOpcio5,ivOpcio6,ivOpcio7,ivOpcio8,ivOpcio9,ivOpcio10;
     private Button btBack, btNext;
     private Intent intentPel1;
     private TestAnswers respostes = new TestAnswers();
@@ -34,18 +37,65 @@ public class PeliculaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pelicula);
 
         ColorGenerator generator = ColorGenerator.DEFAULT;
-        FromPage = TextDrawable.builder().beginConfig().width(70).height(70).endConfig().buildRound("1",generator.getRandomColor());
-        ToPage = TextDrawable.builder().beginConfig().width(70).height(70).endConfig().buildRound("30",generator.getRandomColor());
+        FromPage = TextDrawable.builder().beginConfig().width(65).height(65).endConfig().buildRound("1",generator.getRandomColor());
+        ToPage = TextDrawable.builder().beginConfig().width(65).height(65).endConfig().buildRound("30",generator.getRandomColor());
+
+        for(int i=1; i<=10; i++){
+            botons.add(TextDrawable.builder().beginConfig().width(150).height(150).endConfig().buildRound(String.valueOf(i),generator.getRandomColor()));
+        }
 
         ivFromPage = (ImageView) findViewById(R.id.ivFromPage1);
         ivToPage = (ImageView) findViewById(R.id.ivToPage1);
+
         ivOpcio1 = (ImageView) findViewById(R.id.ivOpcio1Pel1);
+        ivOpcio2 = (ImageView) findViewById(R.id.ivOpcio2Pel1);
+        ivOpcio3 = (ImageView) findViewById(R.id.ivOpcio3Pel1);
+        ivOpcio4 = (ImageView) findViewById(R.id.ivOpcio4Pel1);
+        ivOpcio5 = (ImageView) findViewById(R.id.ivOpcio5Pel1);
+        ivOpcio6 = (ImageView) findViewById(R.id.ivOpcio6Pel1);
+        ivOpcio7 = (ImageView) findViewById(R.id.ivOpcio7Pel1);
+        ivOpcio8 = (ImageView) findViewById(R.id.ivOpcio8Pel1);
+        ivOpcio9 = (ImageView) findViewById(R.id.ivOpcio9Pel1);
+        ivOpcio10 = (ImageView) findViewById(R.id.ivOpcio10Pel1);
+
+        ivOpcio1.setImageDrawable(botons.get(0));
+        ivOpcio2.setImageDrawable(botons.get(1));
+        ivOpcio3.setImageDrawable(botons.get(2));
+        ivOpcio4.setImageDrawable(botons.get(3));
+        ivOpcio5.setImageDrawable(botons.get(4));
+        ivOpcio6.setImageDrawable(botons.get(5));
+        ivOpcio7.setImageDrawable(botons.get(6));
+        ivOpcio8.setImageDrawable(botons.get(7));
+        ivOpcio9.setImageDrawable(botons.get(8));
+        ivOpcio10.setImageDrawable(botons.get(9));
+
+        ivOpcio1.setClickable(true);
+        ivOpcio1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btNext.setEnabled(true);
+
+
+
+            }
+        });
+        ivOpcio2.setImageDrawable(botons.get(1));
+        ivOpcio3.setImageDrawable(botons.get(2));
+        ivOpcio4.setImageDrawable(botons.get(3));
+        ivOpcio5.setImageDrawable(botons.get(4));
+        ivOpcio6.setImageDrawable(botons.get(5));
+        ivOpcio7.setImageDrawable(botons.get(6));
+        ivOpcio8.setImageDrawable(botons.get(7));
+        ivOpcio9.setImageDrawable(botons.get(8));
+        ivOpcio10.setImageDrawable(botons.get(9));
 
         ivFromPage.setImageDrawable(FromPage);
         ivToPage.setImageDrawable(ToPage);
 
         btBack = (Button) findViewById(R.id.btBackPel1);
         btNext = (Button) findViewById(R.id.btNextPel1);
+
+        btNext.setEnabled(false);
 
 
         //Enrere
@@ -85,21 +135,6 @@ public class PeliculaActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
-        //Part particular ^^^^^^^********************^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(getBaseContext(),
-                R.animator.selecciobototest);
-        set.setTarget(ivOpcio1);
-        set.start();
-
-        /*Animation test = AnimationUtils.loadAnimation(this,R.animator.selecciobototest);
-        ivOpcio1.startAnimation(test);*/
 
 
 
