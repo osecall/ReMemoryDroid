5de5d0e0ba3781ef5aa04380f8eb9b99d6e79ab1
osecall <osecall@uoc.edu>
2017-04-09 10:09:08 +0200

    Emotions added

--- app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/QuestionariActivity2.java
+++ app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/QuestionariActivity2.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.IdRes;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -24,7 +22,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -270,6 +267,136 @@ public class QuestionariActivity2 extends AppCompatActivity {
     }
 
     /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class Fragment3 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public Fragment3() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Fragment3 newInstance(int sectionNumber) {
+            Fragment3 fragment = new Fragment3();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            final View rootView = inflater.inflate(R.layout.fragment3_questionari_activity2, container, false);
+            Button btNextPeople = (Button) rootView.findViewById(R.id.btNextEmotions);
+            Button btBackPeople = (Button) rootView.findViewById(R.id.btBackPeople3);
+
+            final RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.rgPeople3);
+
+            radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+
+                    //Busquem quin radioButton s'ha seleccionat
+                    int radioButtonID = radioGroup.getCheckedRadioButtonId();
+                    //Recuperem l'element ara que tenim el identificador per a la classe R (id)
+                    RadioButton rb = (RadioButton) rootView.findViewById(radioButtonID);
+
+                    Toast.makeText(getContext(), rb.getText().toString(),
+                            Toast.LENGTH_LONG).show();
+
+                    //Guardem resposta obteniguda
+                    respostes_recuperades.setPreguntesPersones_Relacio(rb.getText().toString());
+
+                }
+
+            });
+
+            btBackPeople.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mViewPager.setCurrentItem(2);
+                }
+            });
+            btNextPeople.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mViewPager.setCurrentItem(4);
+                }
+            });
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class Fragment4 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public Fragment4() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Fragment4 newInstance(int sectionNumber) {
+            Fragment4 fragment = new Fragment4();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            final View rootView = inflater.inflate(R.layout.fragment4_questionari_activity2, container, false);
+            Button btNextEmotions = (Button) rootView.findViewById(R.id.btNextEmotions);
+            Button btBackEmotions = (Button) rootView.findViewById(R.id.btBackEmotions);
+
+
+
+
+
+
+            //Guardem resposta obteniguda
+            respostes_recuperades.setPreguntesEmocions_Observades(null);
+            respostes_recuperades.setPreguntesEmocions_Propies(null);
+
+
+
+            btBackEmotions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mViewPager.setCurrentItem(2);
+                }
+            });
+            btNextEmotions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mViewPager.setCurrentItem(4);
+                }
+            });
+
+            return rootView;
+        }
+    }
+
+    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -289,6 +416,12 @@ public class QuestionariActivity2 extends AppCompatActivity {
             if(position==1){
                 return QuestionariActivity2.Fragment2.newInstance(position + 1);
             }
+            if(position==2){
+                return QuestionariActivity2.Fragment3.newInstance(position + 1);
+            }
+            if(position==3){
+                return QuestionariActivity2.Fragment4.newInstance(position + 1);
+            }
 
 
             return PlaceholderFragment.newInstance(position + 1);
@@ -296,8 +429,7 @@ public class QuestionariActivity2 extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 2;
+            return 4;
         }
 
         @Override
@@ -307,6 +439,10 @@ public class QuestionariActivity2 extends AppCompatActivity {
                     return getString(R.string.PeopleActions);
                 case 1:
                     return getString(R.string.AgesPeople);
+                case 2:
+                    return getString(R.string.HowManyPeople);
+                case 3:
+                    return getString(R.string.EmotionsDuringEpisode);
 
             }
             return null;
