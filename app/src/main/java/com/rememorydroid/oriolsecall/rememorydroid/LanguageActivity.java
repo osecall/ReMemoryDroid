e8ac2ea57633e41e13adfe0fe2a8e247e7650580
osecall <osecall@uoc.edu>
2017-05-03 02:42:54 +0200

    Es comproba connexió a la xarxa

--- app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/LanguageActivity.java
+++ app/src/main/java/com/rememorydroid/oriolsecall/rememorydroid/LanguageActivity.java
@@ -1,14 +1,22 @@
 package com.rememorydroid.oriolsecall.rememorydroid;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.net.ConnectivityManagerCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.Manifest;
 
@@ -103,6 +111,20 @@ public class LanguageActivity extends AppCompatActivity {
             }
         }
 
+        if(!isThereInternet()){
+            AlertDialog.Builder Dialeg = new AlertDialog.Builder(LanguageActivity.this);
+            Dialeg.setCancelable(true).setMessage("Es necessita INTERNET per funcionar l'aplicació")
+                    .setNeutralButton("D'acord", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            dialogInterface.cancel();
+                            LanguageActivity.this.finish();
+                            startActivity(new Intent(Intent.ACTION_MAIN));
+                        }
+                    }).show();
+        }
+
         btcaIdioma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -191,5 +213,21 @@ public class LanguageActivity extends AppCompatActivity {
         }
     }
 
+
+    private boolean isThereInternet(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo actNetInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo actNetInfo2 = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if(actNetInfo!=null){
+            if(actNetInfo.isConnected()) return true;
+        }
+        else if (actNetInfo2!=null){
+            if(actNetInfo2.isConnected()) return true;
+        }
+        return false;
+
+    }
 
 }
